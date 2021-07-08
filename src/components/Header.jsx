488681800820c37367e4a37f9dc9f29a75dda1b4
sn <nikolasalon@gmail.com>
2021-07-08 15:30:04 +0300

    reated a few components

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Header extends React.Component{
+    state = {
+        rating: '★★★',
+        price: 35,
+        until: '10.07.2020'
+
+    }
+    render() {
+        return(
+            <div className="header-logo">
+                <p className='rating'>Рейтинг: {this.state.rating}</p>
+                {this.state.price !== '' ? <p className='price-logo'> {this.state.price}</p>: null}
+            </div>
+        )
+    }
+}
+
+export default Header
---
+++ src/components/Router.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+
+export default function () {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/"component={} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
